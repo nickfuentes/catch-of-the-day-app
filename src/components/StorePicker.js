86edd59d38f687510b9a3f7495e5e7eceb426dcd
Nickolas Fuentes <nickfuentes24@gmail.com>
2019-09-18 14:18:37 -0500

    Added a function to grab the value of the store and then go to that route.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,26 @@
 import React from "react"
+import { getFunName } from "../helpers"
 
 class StorePicker extends React.Component {
+  myInput = React.createRef()
+
+  goToStore = event => {
+    event.preventDefault()
+    const storeName = this.myInput.current.defaultValue
+    this.props.history.push(`/store/${storeName}`)
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" require placeholder="Store Name"></input>
+        <input
+          type="text"
+          ref={this.myInput}
+          require
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        ></input>
         <button type="submit">Visit Store</button>
       </form>
     )
