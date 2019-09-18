afc16fc32d87aab9fa6768939cb2c7eef72d9f39
Nickolas Fuentes <nickolasfuentes@Nickolass-MacBook-Pro.local>
2019-09-18 12:33:03 -0500

    Added some components to lay out the menu design.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import React from "react"
+import { render } from "react-dom"
+import "./css/style.css"
 
-class StorePicker extends React.Component {}
+import StorePicker from "./components/StorePicker"
+import App from "./components/App"
+
+render(<App />, document.querySelector("#main"))
