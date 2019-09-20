ecb957d55b1d0c890d1a3aae2f123aed55abf1a8
Nickolas Fuentes <nickfuentes24@gmail.com>
2019-09-20 02:14:06 -0500

    Able to render the order of fish and price in the order component.

--- src/components/App.js
+++ src/components/App.js
@@ -26,6 +26,14 @@ class App extends React.Component {
     })
   }
 
+  addToOrder = key => {
+    const order = { ...this.state.order }
+    order[key] = order[key] + 1 || 1
+    this.setState({
+      order: order
+    })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -33,11 +41,16 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
